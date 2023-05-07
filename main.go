249fbf1e2407db78abb842fabf731aaa904e6c3b
Bakarseck <seck.bakar@ugb.edu.sn>
2023-05-07 18:13:58 +0000

    Update

--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
+func gcd(a, b int) int {
+	if a%b == 0 {
+		return b
+	}
+	return gcd(b, a%b)
+}
+
 func ConvertBase(n, base int) string {
-	if n < base {
+	if base > 10 && n > 10 {
+		if n < base {
+			return string(n - 10 + 'A')
+		}
+	} else if n > 10 {
 		return string(n + '0')
+	} 
+	if base > 10 && n > 10 {
+		return ConvertBase(n/base, base) + string(n%16 - 10 + 'A')
 	}
 	return ConvertBase(n/base, base) + string(n%base + '0')
 }
@@ -14,5 +28,9 @@ func ConvertBase(n, base int) string {
 */
 
 func main() {
-	fmt.Println(ConvertBase(7, 7))
+	a := ConvertBase(255, 16)
+	fmt.Println(a)
+
+	b := gcd(2, 0)
+	fmt.Println(b)
 }
