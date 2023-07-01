0c3695be70399ac9553ad4db9022483ba88be0d8
bakseck <seck.bakar@ugb.edu.sn>
2023-07-01 17:41:15 +0000

    groupie-tracker

--- groupie/lib/models/locations.go
+++
@@ -1,9 +0,0 @@
-package groupietracker
-
-type LocationModel struct {
-	Index []struct {
-		Id        int      `json:"id"`
-		Locations []string `json:"locations"`
-		Dates     string   `json:"dates"`
-	} `json:"index"`
-}
